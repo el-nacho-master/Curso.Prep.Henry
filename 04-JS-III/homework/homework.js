f64976058ddd6a44504c6e5dd5785c0386cdac85
el-nacho-master <quirogaveraignacio@gmail.com>
2020-11-09 16:16:48 -0300

    homeworks actualizados

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,16 +3,20 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un array
   // Tu código:
+  return array[0]
 }
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  
+  return array[array.length-1]
 }
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  return array.length
 }
 
 function incrementarPorUno(array) {
@@ -20,12 +24,19 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+  for (let index = 0; index < array.length; index++) {
+    array[index] = array[index]+1;
+    
+  }
+  return array
 }
 
 function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+  array.push(elemento)
+  return array
 }
 
 function agregarItemAlComienzoDelArray(array, elemento) {
@@ -33,6 +44,8 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+  array.unshift(elemento)
+  return array
 }
 
 function dePalabrasAFrase(palabras) {
@@ -41,30 +54,67 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+  var cadena=""
+  for (let index = 0; index < palabras.length; index++) {
+    cadena = cadena + palabras[index];
+    if(index !== palabras.length-1)
+    cadena= cadena+" "
+    
+  }
+  return cadena
 }
 
 function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  var bandera=0
+  for (let index = 0; index < array.length; index++) {
+    if(array[index]===elemento){
+      bandera=1
+    }
+  }
+  if (bandera==1) {
+    return true
+  } else {
+    return false
+    
+  }
 }
 
 function agregarNumeros(numeros) {
   // "array" debe ser una matriz de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+  suma=0
+  for (let index = 0; index < numeros.length; index++) {
+    suma = suma+ numeros[index];
+    }
+    return suma;
 }
 
 function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+  suma=0
+  for (let index = 0; index < resultadosTest.length; index++) {
+    suma = suma+ resultadosTest[index];
+    }
+    return suma/resultadosTest.length;
+
 }
 
 function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+  var mayor=0
+  for (let index = 0; index <numeros.length; index++) {
+    if(numeros[index]>mayor)
+    mayor=numeros[index]
+  }
+  return mayor 
 }
 
 function multiplicarArgumentos() {
@@ -72,6 +122,15 @@ function multiplicarArgumentos() {
   // Si no se pasan argumentos devuelve 0
   // Si se pasa un argumento, simplemente devuélvelo
   // Tu código:
+  var multi=1
+for (let index = 0; index < arguments.length; index++) {
+ 
+  multi =multi*arguments[index]
+}
+if(arguments.length===0)
+multi=0
+return multi
+
 }
 
 // No modificar nada debajo de esta línea
--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,6 +6,14 @@ function crearGato(nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+  var cat = {
+    nombre:'nombre',
+    edad:'edad',
+    meow:function(){
+      return 'Meow!'
+    },
+  };
+  return cat
 }
 
 
@@ -14,6 +22,7 @@ function agregarPropiedad(objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+ 
 }
 
 function invocarMetodo(objeto, metodo) {
@@ -27,7 +36,8 @@ function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
   // "objetoMisterioso" tiene una propiedad llamada "numeroMisterioso"
   // Multiplica el numeroMisterioso por 5 y devuelve el producto
   // Tu código:
-
+  
+  return mysteryBox.numeroMisterio*2;
 }
 
 function eliminarPropiedad(objeto, propiedad) {
